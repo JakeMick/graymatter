abffa865532335310f4640e43415d711c862cbde
Jacob Mick <anon.biohacker@gmail.com>
2014-02-23 12:15:10 -0600

    Class-based tests

--- nn/tests/mlp_test.py
+++ nn/tests/mlp_test.py
@@ -3,37 +3,33 @@ from sklearn.metrics import log_loss
 from ..mlp import MLP
 
 
-def test_if_fit_classification():
-    model = MLP()
-    X, y = make_classification()
-    model.fit(X, y)
+class TestFitting(object):
 
+    def __init__(self):
+        self.X_cl, self.y_cl = make_classification(100)
+        self.X_re, self.y_re = make_classification(100)
 
-def test_if_fit_regression():
-    model = MLP()
-    X, y = make_regression()
-    model.fit(X, y)
+    def test_if_fit_classification():
+        model = MLP()
+        model.fit(self.X_cl, self.y_cl)
+        assert(model.type_of_target_ == 'binary')
 
+    def test_if_fit_regression(self):
+        model = MLP()
+        model.fit(self.X_re, self.y_re)
+        assert(model.type_of_target_ == 'continuous')
 
-def test_sigmoid():
-    model = MLP(hidden_layer_type='sigm')
-    X, y = make_classification()
-    model.fit(X, y)
+    def test_sigmoid(self):
+        model = MLP(hidden_layer_type='sigm')
+        model.fit(self.X_cl, self.y_cl)
 
+    def test_dropout(self):
+        model = MLP(dropout='True')
+        model.fit(self.X_cl, self.y_cl)
 
-def test_dropout():
-    model = MLP(dropout='True')
-    X, y = make_classification()
-    model.fit(X, y)
-
-
-def test_accuracy():
-    model = MLP()
-    X, y = make_classification(100)
-    model.fit(X[:50], y[:50])
-    y_pred = model.predict_proba(X[50:])
-    ll = log_loss(y[50:], y_pred)
-    assert(ll < .05)
-
-if __name__ == "__main__":
-    check_if_fit()
+    def test_accuracy(self):
+        model = MLP()
+        model.fit(self.X_cl[:50], self.y_cl[:50])
+        y_pred = model.predict_proba(self.X_cl[50:])
+        ll = log_loss(self.y_cl[50:], y_pred)
+        assert(ll < .05)
